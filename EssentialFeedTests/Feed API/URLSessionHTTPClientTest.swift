58d02488f1d62012011d9e28792619a720810bb2
Wilson Munoz <wilsonilo@gmail.com>
2025-05-06 20:21:25 -0500

    Using protocols for session and task as a method for testing.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -6,41 +6,102 @@
 //
 
 import XCTest
+import EssentialFeed
+
+protocol HTTPSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> HTTPSessionTask
+}
+
+protocol HTTPSessionTask {
+    func resume()
+}
 
 class URLSessionHTTPClient {
-    private let session: URLSession
+    private let session: HTTPSession
     
-    init(session: URLSession) {
+    init(session: HTTPSession) {
         self.session = session
     }
     
-    func get(from:URLRequest){
-        session.dataTask(with: from) { data, response, error in
-            //
-        }
+    func get(from:URL, completion:@escaping (HTTPClientResponse)->Void){
+        session.dataTask(with: from) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 class URLSessionHTTPClientTest: XCTestCase {
-    func test_getFromURL_createDataTaskWithURL(){
-        let url = URL(string:"http://any-url.com")!
-        let request = URLRequest(url: url)
-        let session = URLSessionSpy()
+    
+    func test_getFromURL_resumeDataTaskWithURL(){
+        let url = URL(string:"http://any-other.com")!
+        let session = HTTPSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
         let sut = URLSessionHTTPClient(session: session)
-        sut.get(from:request)
-        XCTAssertEqual(session.receivedURLs, [url])
+        sut.get(from: url) { _ in
+            
+        }
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
+    func test_getFromURL_failsOnRequestError(){
+        let url = URL(string:"http://any-other.com")!
+        let session = HTTPSessionSpy()
+        let error = NSError(domain: "any error", code: 1)
+        session.stub(url:url, error:error)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        let expectation = expectation(description: "Wait for url completion")
+        sut.get(from: url) { result in
+            switch result {
+            case .success(_, _):
+                XCTFail("Expected \(error) but got \(result)")
+            case .failure(let receivedError as NSError):
+                XCTAssertEqual(receivedError, error)
+            @unknown default:
+                fatalError("Unknown result")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
     
     // MARK: - Helpers
-    private class URLSessionSpy:URLSession {
-        var receivedURLs = [URL]()
-        override func dataTask(with request: URLRequest) -> URLSessionDataTask {
-            self.receivedURLs.append(request.url!)
-            return FakeURLSessionDataTask()
+    private class HTTPSessionSpy:HTTPSession {
+
+        private var stubs = [URL: Stub]()
+        private struct Stub {
+            let task:HTTPSessionTask
+            let error:Error?
+        }
+        func stub(
+            url:URL,
+            task:HTTPSessionTask = FakeURLSessionDataTask(),
+            error:Error? = nil
+        ){
+            stubs[url] = Stub(task: task, error: error)
+        }
+        
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> HTTPSessionTask {
+            guard let stub = stubs[url] else {
+                fatalError("Couldn't find stub for \(url)")
+            }
+            completionHandler(nil, nil, stub.error)
+            return stub.task
         }
         
     }
     
-    private class FakeURLSessionDataTask:URLSessionDataTask {}
+    private class FakeURLSessionDataTask:HTTPSessionTask {
+        func resume(){}
+    }
+    private class URLSessionDataTaskSpy:HTTPSessionTask {
+        var resumeCallCount = 0
+        func resume() {
+            resumeCallCount += 1
+        }
+    }
     
 }
